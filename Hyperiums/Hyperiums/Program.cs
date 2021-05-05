6a7a933f644ec7d542b0b31aa1a2a4769c23c088
pontailler.jb@gmail.com <pontailler.jb@gmail.com>
2021-05-05 15:39:14 +0200

    add README.md delete txt

--- Hyperiums/Hyperiums/Program.cs
+++ Hyperiums/Hyperiums/Program.cs
@@ -8,8 +8,12 @@ namespace Hyperiums
         static void Main(string[] args)
         {
             //Extraction and display file planet.txt
-            string[] planets = File.ReadAllLines("C:/Users/ponta/Desktop/La manu/Git/Hyperiums/log/planet.txt").Skip('#');
+            string[] planets = File.ReadAllLines("C:/Users/JB/Desktop/La Manu/Git/Hyperiums_Tools/log/planet.txt");
             Console.WriteLine(planets);
+
+            var lines = File.ReadLines(ofd.FileName);
+            foreach (string line in lines.Skip(2))
+                Trace.WriteLine(line);
 
             foreach (string line in planets)
             Console.WriteLine(line);
